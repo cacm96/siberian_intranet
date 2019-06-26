200c832b2e28a9387f37578ed5a5962344bd5f02
Anderson Diaz <andersondejesus2011@gmail.com>
2019-06-26 04:07:09 -0430

    Restructure las carpetas creando masters y configures, plantilla de crud creada en system

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,12 +11,12 @@ import { ErrorComponent } from './components/error/error.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { LogoutComponent } from './components/auth/logout/logout.component';
 
-import { UserFatherComponent } from './components/user/user-father/user-father.component';
-import { UsersComponent } from './components/user/users/users.component';
-import { UserCreateComponent } from './components/user/user-create/user-create.component';
-import { UserEditComponent } from './components/user/user-edit/user-edit.component';
-import { UserShowComponent } from './components/user/user-show/user-show.component';
-import { UserDeleteComponent } from './components/user/user-delete/user-delete.component';
+import { UserFatherComponent } from './components/masters/configure-user/user/user-father/user-father.component';
+import { UsersComponent } from './components/masters/configure-user/user/users/users.component';
+import { UserCreateComponent } from './components/masters/configure-user/user/user-create/user-create.component';
+import { UserEditComponent } from './components/masters/configure-user/user/user-edit/user-edit.component';
+import { UserShowComponent } from './components/masters/configure-user/user/user-show/user-show.component';
+import { UserDeleteComponent } from './components/masters/configure-user/user/user-delete/user-delete.component';
 
 import { ProfileFatherComponent } from './components/profile/profile-father/profile-father.component';
 import { ProfilesComponent } from './components/profile/profiles/profiles.component';
@@ -25,25 +25,31 @@ import { ProfileEditComponent } from './components/profile/profile-edit/profile-
 import { ProfileShowComponent } from './components/profile/profile-show/profile-show.component';
 import { ProfileDeleteComponent } from './components/profile/profile-delete/profile-delete.component';
 
-import { SystemFatherComponent } from './components/system/system-father/system-father.component';
-import { SystemsComponent } from './components/system/systems/systems.component';
-import { SystemCreateComponent } from './components/system/system-create/system-create.component';
-import { SystemEditComponent } from './components/system/system-edit/system-edit.component';
-import { SystemShowComponent } from './components/system/system-show/system-show.component';
-import { SystemDeleteComponent } from './components/system/system-delete/system-delete.component';
+import { SystemFatherComponent } from './components/masters/configure-system/system/system-father/system-father.component';
+import { SystemsComponent } from './components/masters/configure-system/system/systems/systems.component';
+import { SystemCreateComponent } from './components/masters/configure-system/system/system-create/system-create.component';
+import { SystemEditComponent } from './components/masters/configure-system/system/system-edit/system-edit.component';
+import { SystemShowComponent } from './components/masters/configure-system/system/system-show/system-show.component';
+import { SystemDeleteComponent } from './components/masters/configure-system/system/system-delete/system-delete.component';
 
-import { FunctionFatherComponent } from './components/function/function-father/function-father.component';
-import { FunctionsComponent } from './components/function/functions/functions.component';
-import { FunctionCreateComponent } from './components/function/function-create/function-create.component';
-import { FunctionEditComponent } from './components/function/function-edit/function-edit.component';
-import { FunctionShowComponent } from './components/function/function-show/function-show.component';
-import { FunctionDeleteComponent } from './components/function/function-delete/function-delete.component';
+import { FunctionFatherComponent } from './components/masters/configure-system/function/function-father/function-father.component';
+import { FunctionsComponent } from './components/masters/configure-system/function/functions/functions.component';
+import { FunctionCreateComponent } from './components/masters/configure-system/function/function-create/function-create.component';
+import { FunctionEditComponent } from './components/masters/configure-system/function/function-edit/function-edit.component';
+import { FunctionShowComponent } from './components/masters/configure-system/function/function-show/function-show.component';
+import { FunctionDeleteComponent } from './components/masters/configure-system/function/function-delete/function-delete.component';
 
 import { IndexManagerComponent } from './components/dashboard/manager/index-manager/index-manager.component';
 import { IndexAdminComponent } from './components/dashboard/admin/index-admin/index-admin.component';
 import { IndexLenderComponent } from './components/dashboard/lender/index-lender/index-lender.component';
 import { IndexClienteComponent } from './components/dashboard/client/index-cliente/index-cliente.component';
 
+import { CategoryFatherComponent } from './components/masters/configure-catalogue/category-father/category-father.component';
+import { CategorysComponent } from './components/masters/configure-catalogue/categorys/categorys.component';
+import { CategoryCreateComponent } from './components/masters/configure-catalogue/category-create/category-create.component';
+import { CategoryEditComponent } from './components/masters/configure-catalogue/category-edit/category-edit.component';
+import { CategoryShowComponent } from './components/masters/configure-catalogue/category-show/category-show.component';
+import { CategoryDeleteComponent } from './components/masters/configure-catalogue/category-delete/category-delete.component';
 
 // Array de rutas
 const appRoutes: Routes = [
@@ -95,10 +101,24 @@ const appRoutes: Routes = [
 			{path: 'delete/:id', component: FunctionDeleteComponent},
 		]
 	},
+
+
+	{path: 'category', component: CategoryFatherComponent, canActivate: [AuthGuard],
+		children:
+		[
+			{path: '', component: CategorysComponent},
+			{path: 'create', component: CategoryCreateComponent},
+			{path: 'edit/:id', component: CategoryEditComponent},
+			{path: 'show/:id', component: CategoryShowComponent},
+			{path: 'delete/:id', component: CategoryDeleteComponent},
+		]
+	},
+
 	{path: 'dashboardClient', component: IndexClienteComponent, canActivate: [AuthGuard]},
 	{path: 'dashboardLender', component: IndexLenderComponent, canActivate: [AuthGuard]},
 	{path: 'dashboardManager', component: IndexManagerComponent, canActivate: [AuthGuard]},
 	{path: 'dashboardAdmin', component: IndexAdminComponent, canActivate: [AuthGuard]},
+
 
 	
 
---
+++ src/app/components/masters/configure-catalogue/category-delete/category-delete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sib-category-delete',
+  templateUrl: './category-delete.component.html',
+  styleUrls: ['./category-delete.component.scss']
+})
+export class CategoryDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
