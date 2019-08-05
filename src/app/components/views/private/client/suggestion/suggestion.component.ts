fdaac21f96515759fa6b7278facf367de72ed070
Anderson Diaz <andersondejesus2011@gmail.com>
2019-08-04 23:18:57 -0430

    Refinando diseño y maquetación de los cards en los dashboard de client y lender, preparando las rutas para el ciclo funcional del client, entidades nuevas y dejando las viejas aun

---
+++ src/app/components/views/private/client/suggestion/suggestion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sib-suggestion',
+  templateUrl: './suggestion.component.html',
+  styleUrls: ['./suggestion.component.scss']
+})
+export class SuggestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
