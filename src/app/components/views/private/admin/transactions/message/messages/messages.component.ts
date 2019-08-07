dd194ab877b530bc1a081de74ae3126632b33a9b
@laguyu <wilmar88880@gmail.com>
2019-08-07 02:06:45 -0430

    arreglando los cruds de catologo,catalogo variedad detalle,actividad,categoria,equipo/infraestructura y creando nuevos cruds mensaje, chat salon y chat salon usuario

--- src/app/components/views/private/admin/transactions/message/messages/messages.component.ts
+++ src/app/components/views/private/admin/transactions/message/messages/messages.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DialogService } from '../../../../../../../core/services/dialog.service';
+import { SnackBarService } from '../../../../../../../core/services/snack-bar.service';
+
+export interface MessageData {
+  id: string;
+  chatRoomId: string;
+  userId: string;
+  body: string;
+  
+}
 
 @Component({
   selector: 'sib-messages',
@@ -7,9 +20,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  public message:any[];
+	displayedColumns: string[] = ['id','chatRoomId','userId', 'body','edit','delete'];
+  dataSource: MatTableDataSource<MessageData>;
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private dialogService: DialogService,
+    private snackBar: SnackBarService
+  )
+  
+  {
+    this.message = [
+      {id:"1",chatRoomId:"Chat 1" ,userId:"andre@gmail.com",body:"Mensaje 1"},
+      {id:"2", chatRoomId:"Chat 2",userId:"rey@gmail.com",body:"Mensaje 2"},
+      {id:"3", chatRoomId:"Chat 3",userId:"yasm@gmail.com",body:"Mensaje 3"},
+    ];
+      this.dataSource = new MatTableDataSource(this.message);
+   }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
   }
 
+  applyFilter(filterValue: string) {
+		this.dataSource.filter = filterValue.trim().toLowerCase();
+
+		if (this.dataSource.paginator) {
+		  this.dataSource.paginator.firstPage();
+		}
+	}
+
+	onDelete(id){
+		this.dialogService.openConfirmDialog('¿Estás seguro de eliminar el Mensaje'+id+' ?').afterClosed().subscribe(res=>{
+			if (res==true) {
+				console.log(id);
+				this.snackBar.openSnackBar('Eliminado Correctamente','¿Deshacer?').onAction().subscribe(() => {
+				  console.log('Recuperado');
+				});
+			}else{
+				console.log(res);
+			}
+		});
+	}
 }
+
