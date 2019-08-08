42b6ad55cd352e64968eca370b65d2087ec53e43
marianyelinany <marianyelinany04@gmail.com>
2019-08-08 12:38:42 -0400

    CRUD Promocion

--- src/app/components/views/private/admin/configurations/promotion/promotions/promotions.component.ts
+++ src/app/components/views/private/admin/configurations/promotion/promotions/promotions.component.ts
@@ -42,7 +42,7 @@ export class PromotionsComponent implements OnInit {
     this.promotion = [
       {id:"1" ,name:"Descuento Mes de las madres",description:"Descuento por el mes de mayo en el mantenimiento de Neveras",percentDiscount:"15%",idSubcategory:"Nevera",date_init: "01-05-2020",date_end: "31-05-2020",image:"url1",status:"A",},
       {id:"2" ,name:"Descuento de linea blanca",description:"Descuento en todas nuestros servicio de linea blanca",percentDiscount:"10%",idSubcategory:"cocina",date_init: "01-06-2020",date_end: "05-06-2020",image:"url1",status:"A",},
-      {id:"3" ,name:"Descuento decembrinas",description:"Descuento en el mes de diciembre en servicio de pintura",percentDiscount:"15%",idSubcategory:"Pared",date_init: "31-12-2019",date_end: "31-12-2019",image:"url1",status:"A",},
+      {id:"3" ,name:"Descuento decembrinas ",description:"Descuento en el mes de diciembre en servicio de pintura",percentDiscount:"15%",idSubcategory:"Pared",date_init: "31-12-2019",date_end: "31-12-2019",image:"url1",status:"A",},
     ];
 
   this.dataSource = new MatTableDataSource(this.promotion);
