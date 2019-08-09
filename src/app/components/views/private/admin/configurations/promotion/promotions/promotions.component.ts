1c42639c3e9632192707cae03f744c35f24604af
Anderson Diaz <andersondejesus2011@gmail.com>
2019-08-08 22:59:46 -0430

    Armando las vistas de solicitud luego del stepper

--- src/app/components/views/private/admin/configurations/promotion/promotions/promotions.component.ts
+++ src/app/components/views/private/admin/configurations/promotion/promotions/promotions.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromotionsComponent implements OnInit {
 
-  public promotion:any[];
+/*  public promotion:any[];
 	displayedColumns: string[] = ['id', 'name', 'description','percentDiscount','idSubcategory','date_init','date_end','image','status','edit','delete'];
 	dataSource: MatTableDataSource<PromotionData>;
 
@@ -23,7 +23,7 @@ export class PromotionsComponent implements OnInit {
       {id:"1" ,name:"Descuento Mes de las madres",description:"Descuento por el mes de mayo en el mantenimiento de Neveras",percentDiscount:"15%",idSubcategory:"Nevera",date_init: "01-05-2020",date_end: "31-05-2020",image:"url1",status:"A",},
       {id:"2" ,name:"Descuento de linea blanca",description:"Descuento en todas nuestros servicio de linea blanca",percentDiscount:"10%",idSubcategory:"cocina",date_init: "01-06-2020",date_end: "05-06-2020",image:"url1",status:"A",},
       {id:"3" ,name:"Descuento decembrinas ",description:"Descuento en el mes de diciembre en servicio de pintura",percentDiscount:"15%",idSubcategory:"Pared",date_init: "31-12-2019",date_end: "31-12-2019",image:"url1",status:"A",},
-    ];
+    ];*/
 
   ngOnInit() {
   }
