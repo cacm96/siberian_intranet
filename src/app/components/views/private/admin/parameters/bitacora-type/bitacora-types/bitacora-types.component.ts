28d718a31050049cf8ae58dcf5ca3238744d0baa
Keyvin Díaz <keyvin.95@gmail.com>
2019-08-11 01:49:12 -0430

    CRUD's Incidence, MotiveIncidence, CalendarType and CalendarWorker

--- src/app/components/views/private/admin/parameters/bitacora-type/bitacora-types/bitacora-types.component.ts
+++ src/app/components/views/private/admin/parameters/bitacora-type/bitacora-types/bitacora-types.component.ts
@@ -1,10 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DialogService } from '../../../../../../../core/services/dialog.service';
+import { SnackBarService } from '../../../../../../../core/services/snack-bar.service';
+
+export interface ServiceDetailData {
+  request: string;
+  revision: string;
+  orderService: string;
+  Presupuesto: string;
+  orderServiceWaranty: string;
+  Reclamo: string;
+  revisionWaranty : string;
+}
 
 @Component({
   selector: 'sib-bitacora-types',
   templateUrl: './bitacora-types.component.html',
   styleUrls: ['./bitacora-types.component.scss']
 })
+
 export class BitacoraTypesComponent implements OnInit {
 
   constructor() { }
--- src/app/components/views/private/admin/parameters/motive-incidence/motive-incidences/motive-incidences.component.ts
+++ src/app/components/views/private/admin/parameters/motive-incidence/motive-incidences/motive-incidences.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DialogService } from '../../../../../../../core/services/dialog.service';
+import { SnackBarService } from '../../../../../../../core/services/snack-bar.service';
+
+export interface motiveIncidenceData {
+  id: string;
+  name: string;
+  description: string;
+  status: string;
+}
 
 @Component({
   selector: 'sib-motive-incidences',
@@ -7,9 +19,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MotiveIncidencesComponent implements OnInit {
 
-  constructor() { }
+  public motiveIncidence:any[];
+  displayedColumns: string[] = [ 
+    'id',
+    'name',
+    'description',
+    'status',
+    'edit',
+    'delete'];
+  dataSource: MatTableDataSource<motiveIncidenceData>;
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+	@ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private dialogService: DialogService,
+		private snackBar: SnackBarService
+  ) { 
+    this.motiveIncidence = [
+      {id:"1",name:"Posponer",description:"Cambio de fecha indisposición",status:"A"},
+      {id:"2",name:"Reposo",description:"Cambio de fecha por enfermedad",status:"E"},
+    ];
+
+  this.dataSource = new MatTableDataSource(this.motiveIncidence);
+  }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
   }
+
+  applyFilter(filterValue: string) {
+		this.dataSource.filter = filterValue.trim().toLowerCase();
+
+		if (this.dataSource.paginator) {
+		  this.dataSource.paginator.firstPage();
+		}
+	}
+
+	onDelete(id){
+		this.dialogService.openConfirmDialog('¿Estas seguro de eliminar el Motivo de Incidencia '+id+' ?').afterClosed().subscribe(res=>{
+			if (res==true) {
+				console.log(id);
+				this.snackBar.openSnackBar('Eliminado Correctamente','¿Deshacer?').onAction().subscribe(() => {
+				  console.log('Recuperado');
+				});
+			}else{
+				console.log(res);
+			}
+		});
+	}
 
 }
