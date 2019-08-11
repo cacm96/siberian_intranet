8d6bd8b1bc11ce1962d89c43bd01bce38715a02f
Anderson Diaz <andersondejesus2011@gmail.com>
2019-08-11 00:54:30 -0430

    Efecto del formulario otros y de los prestadores disponibles

--- src/app/components/views/private/client/catalog/others/others.component.ts
+++ src/app/components/views/private/client/catalog/others/others.component.ts
@@ -7,13 +7,14 @@ import { SnackBarService } from '../../../../../../core/services/snack-bar.servi
   styleUrls: ['./others.component.scss']
 })
 export class OthersComponent implements OnInit {
+	selected = '';
 
-  constructor(private snackBar: SnackBarService) { }
+  	constructor(private snackBar: SnackBarService) { }
   
 
-  ngOnInit() {
-  }
-  onEnviar(){
-  	this.snackBar.openSnackBar('Datos enviados correctamente. Te notificaremos cuando tu solicitud sea aprobada','').onAction().subscribe(() => {});
-  }
+	ngOnInit() {
+	}
+	onEnviar(){
+		this.snackBar.openSnackBar('Datos enviados correctamente. Te notificaremos cuando tu solicitud sea aprobada','').onAction().subscribe(() => {});
+	}
 }
