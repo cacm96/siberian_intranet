06e0cce9f815d43c6f27a319664d3c923ecd5de7
djmikel16 <d.j.mikel16@gmail.com>
2019-08-13 20:08:55 -0400

    Crud Skill-worker terminado fecha de 13082019

--- src/app/components/views/private/admin/configurations/phone/phones/phones.component.ts
+++ src/app/components/views/private/admin/configurations/phone/phones/phones.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import {MatTableDataSource} from '@angular/material/table';
+import { MatTableDataSource} from '@angular/material/table';
 import { DialogService } from '../../../../../../../core/services/dialog.service';
 import { SnackBarService } from '../../../../../../../core/services/snack-bar.service';
 
