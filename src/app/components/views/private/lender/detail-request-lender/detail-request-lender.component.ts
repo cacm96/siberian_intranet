ed686dfacba21175ba6ae26a20bddc9181e1c3f9
@laguyu <wilmar88880@gmail.com>
2019-09-02 10:31:19 -0430

    Creación de vista detalle servicio del prestador

---
+++ src/app/components/views/private/lender/detail-request-lender/detail-request-lender.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sib-detail-request-lender',
+  templateUrl: './detail-request-lender.component.html',
+  styleUrls: ['./detail-request-lender.component.scss']
+})
+export class DetailRequestLenderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
