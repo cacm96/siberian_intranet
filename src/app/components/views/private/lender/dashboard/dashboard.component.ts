5ea73b3492b4415387c50fbf50fc5cf55ed15206
cacm96 <carloscolmenarez1996@gmail.com>
2019-09-24 09:49:38 -0400

    Prestador, calendario y dashboard

--- src/app/components/views/private/lender/dashboard/dashboard.component.ts
+++ src/app/components/views/private/lender/dashboard/dashboard.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
-import localeEs from '@angular/common/locales/es';
+import { Component, ChangeDetectionStrategy, Inject, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
 
+import { startOfDay, endOfDay, subDays, addDays, endOfMonth, isSameDay, isSameMonth, addHours} from 'date-fns';
+import { CalendarEvent, CalendarEventAction, CalendarEventTimesChangedEvent, CalendarView } from 'angular-calendar';
+
+import { registerLocaleData, DOCUMENT } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 registerLocaleData(localeEs);
 
 @Component({
@@ -9,13 +12,29 @@ registerLocaleData(localeEs);
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent {
 
-  
+  view: CalendarView = CalendarView.Month;
 
-  constructor() { }
+  viewDate = new Date();
 
-  ngOnInit() {
+  events: CalendarEvent[] = [
+    {
+      title: 'An event',
+      start: new Date(),
+      // color: colors.red
+    }
+  ];
+
+  private readonly darkThemeClass = 'dark-theme';
+
+  constructor(@Inject(DOCUMENT) private document) {}
+
+ /* ngOnInit(): void {
+    this.document.body.classList.add(this.darkThemeClass);
   }
 
+  ngOnDestroy(): void {
+    this.document.body.classList.remove(this.darkThemeClass);
+  }*/
 }
