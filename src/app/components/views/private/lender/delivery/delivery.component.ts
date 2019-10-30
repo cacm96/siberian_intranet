3735abbeb769fe4ae43d2e3acc44af17998dabac
@laguyu <wilmar88880@gmail.com>
2019-10-30 01:05:34 -0430

    creacion de tablas de presupuesto,ejecucion,entrega y calificación

--- src/app/components/views/private/lender/delivery/delivery.component.ts
+++ src/app/components/views/private/lender/delivery/delivery.component.ts
@@ -1,5 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewChild  } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import { DialogService } from '../../../../../core/services/dialog.service';
+import { SnackBarService } from '../../../../../core/services/snack-bar.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import {Location} from '@angular/common';
 
+export interface DeliveryData {
+  id: string;
+  client:string;
+  equipinfras: string;
+  code: string;
+  date: string;
+  amount: string;
+  startdate: string;
+  enddate: string;
+  services: string;
+  status: string;
+  }
 @Component({
   selector: 'sib-delivery',
   templateUrl: './delivery.component.html',
@@ -7,9 +26,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeliveryComponent implements OnInit {
 
-  constructor() { }
+  public Delivery:any[];
+displayedColumns: string[] = ['id','client','equipinfras','code','amount','startdate','enddate','services','status'];
+  dataSource: MatTableDataSource<DeliveryData>;
+  
+@ViewChild(MatPaginator) paginator: MatPaginator;
+@ViewChild(MatSort) sort: MatSort;
+  constructor(
+    private dialogService: DialogService,
+    private snackBar: SnackBarService,
+    private _route: ActivatedRoute,
+		private _router: Router,
+		private _location: Location
+  ) 
+  {
+    this.Delivery= [
+      {id:"1" ,client: "Anderson Diaz",equipinfras:"Lavadora",code:"1000",amount:"50000",startdate:"12-11-2019",enddate:"13-11-2019",services:"2",status:"executed",},
+      {id:"2" ,client: "Anderson Diaz",equipinfras:"Cocina",code:"2000",amount:"100000",startdate:"12-11-2019",enddate:"14-11-2019",services:"3",status:"executed",},
+    ];
+
+  this.dataSource = new MatTableDataSource(this.Delivery);
+   }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
+
+  applyFilter(filterValue: string) {
+		this.dataSource.filter = filterValue.trim().toLowerCase();
+
+		if (this.dataSource.paginator) {
+		  this.dataSource.paginator.firstPage();
+		}
+	}
+
+	
+  goBack()
+	{ 
+		this._location.back(); 
+	}
 
 }
--- src/app/components/views/private/lender/diagnosis/diagnosis.component.ts
+++ src/app/components/views/private/lender/diagnosis/diagnosis.component.ts
@@ -12,6 +12,15 @@ import { Global } from 'src/app/core/services/global';
 import { Revision } from 'src/app/models/revision';
 import { RevisionService } from 'src/app/core/services/client/revision.service';
 
+export interface DiagnosisData {
+	id: string;
+	client: string;
+	equipinfras: string;
+	location: string;
+	date: string;
+	lender: string;
+	status: string;
+	}
 
 @Component({
   selector: 'sib-diagnosis',
@@ -20,73 +29,39 @@ import { RevisionService } from 'src/app/core/services/client/revision.service';
 })
 export class DiagnosisComponent implements OnInit {
 
-	public revision:any;
-	public revisions: Array < Revision > = new Array < Revision > ();
-	public total:number=0;
-	public userID:string;
-	public message:string;
-	public failedConect:string;
+	
+	
+	public Diagnosis:any[];
 
-	displayedColumns: string[] = ['id', 'equipinfras','location','date','lender','status'];
-	dataSource: MatTableDataSource<Revision>;
+	displayedColumns: string[] = ['id','client','equipinfras','location','date','lender','status'];
+	dataSource: MatTableDataSource<DiagnosisData>;
 
 	@ViewChild(MatPaginator) paginator: MatPaginator;
 	@ViewChild(MatSort) sort: MatSort;
-
 	constructor
 	(
 		private dialogService: DialogService,
 		private snackBar: SnackBarService,
-		private _revisionService: RevisionService,
 		private _route: ActivatedRoute,
 		private _router: Router,
 		private _location: Location
 		)
 	{
+		this.Diagnosis =
+		[
+	      {id:"1" ,client:"Anderson Diaz",equipinfras:"Lavadora",location:"Calle San Rafael",date:"10-11-2019",lender:"Maria Moreno",status:"diagnosticated",},
+	      {id:"1" ,client:"Anderson Diaz",equipinfras:"Cocina",location:"Calle San Rafael",date:"11-11-2019",lender:"Maria Moreno",status:"approved",},
+	    ];
 
+		this.dataSource = new MatTableDataSource(this.Diagnosis);
 	}
 
 	ngOnInit() {
-		this.userID = localStorage.getItem('resID');
-		this.getRevisions(this.userID);
+		this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
 	}
 
-	getRevisions(userID)
-	{
-		this._revisionService.getRevisionUser(userID).subscribe
-		(
-			response =>
-			{
-				if (response.status==true)
-				{
-					this.revisions = response.revisions;
-					this.revisions = this.revisions.filter(revision=>{return revision.status =="approved"});
-					this.total=this.revisions.length;
-					console.log(this.revisions);
-					this.table();
-				}
-				else
-				{
-					this.revisions = [];
-					this.message = response.message.text;
-					console.log(this.message);
-					this.table();
-				}
-
-			},
-			error =>
-			{
-				console.log(<any>error);
-				if(error instanceof HttpErrorResponse)
-				{
-					if(error.status===0)
-					{
-						this.failedConect = Global.failed;
-					}
-				}
-			}
-			)
-	}
+	
 
 	applyFilter(filterValue: string)
 	{
@@ -97,83 +72,7 @@ export class DiagnosisComponent implements OnInit {
 		}
 	}
 
-	table()
-	{
-		this.dataSource = new MatTableDataSource(this.revisions);
-		this.dataSource.paginator = this.paginator;
-		this.dataSource.sort = this.sort;
-	}
-
-	onApproved(id){
-		this.dialogService.openConfirmDialog('¿Estás seguro de aprobar esta Solicitud?').afterClosed().subscribe
-		(
-			response =>
-			{
-				if (response==true)
-				{
-					this.approvedRevision(id);
-				}else
-				{
-					console.log(response);
-				}
-			}
-			);
-	}
-
-
-	approvedRevision(id)
-	{
-		this._revisionService.approve(id).subscribe
-		(
-			response =>
-			{
-				console.log(response);
-				this.message = response.message.text;
-				this.snackBar.openSnackBarSuccess(this.message);
-				this.getRevisions(this.userID);
-			},
-			error =>
-			{
-				console.log(<any>error);
-			}
-			)
-	}
-
-
-	onCancelled(id){
-		this.dialogService.openConfirmDialog('¿Estás seguro de rechazar esta Solicitud?').afterClosed().subscribe
-		(
-			response =>
-			{
-				if (response==true)
-				{
-					this.cancelRevision(id);
-				}else
-				{
-					console.log(response);
-				}
-			}
-			);
-	}
-
-
-	cancelRevision(id)
-	{
-		this._revisionService.cancel(id).subscribe
-		(
-			response =>
-			{
-				console.log(response);
-				this.message = response.message.text;
-				this.snackBar.openSnackBarSuccess(this.message);
-				this.getRevisions(this.userID);
-			},
-			error =>
-			{
-				console.log(<any>error);
-			}
-			)
-	}
+	
 
 	goBack()
 	{ 
