2b31f9ad73c7b5bda4e0f1ed33b27cdd7ca1ecab
Anyami Cornieles <anyamigcr@gmail.com>
2019-11-01 00:06:33 -0400

    Creando crud motive

--- src/app/components/views/private/admin/configurations/parameter/motive/motives/motives.component.ts
+++ src/app/components/views/private/admin/configurations/parameter/motive/motives/motives.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DialogService } from '../../../../../../../../core/services/dialog.service';
+import { SnackBarService } from '../../../../../../../../core/services/snack-bar.service';
+
+export interface Motive {
+  id: string;
+  name: string;
+  description: string;
+  status: string;
+  
+}
 
 @Component({
   selector: 'sib-motives',
@@ -7,9 +20,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MotivesComponent implements OnInit {
 
-  constructor() { }
+  public motive:any[];
+	displayedColumns: string[] = ['id','name','description','status','edit','delete'];
+  dataSource: MatTableDataSource<Motive>;
+  
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+
+  constructor(
+    private dialogService: DialogService,
+    private snackBar: SnackBarService
+  ) 
+  {
+    this.motive = [
+      {id:"1",name:"Marca",description:"La marca que posee el equipo",status:"A"},
+      {id:"2",name:"Modelo",description:"El Modelo que posee el equipo",status:"A"},
+      {id:"3",name:"Año",description:"Año del equipo",status:"A"},
+    ];
+      this.dataSource = new MatTableDataSource(this.motive);
+   }
 
   ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+		this.dataSource.sort = this.sort;
   }
 
+  applyFilter(filterValue: string) {
+		this.dataSource.filter = filterValue.trim().toLowerCase();
+
+		if (this.dataSource.paginator) {
+		  this.dataSource.paginator.firstPage();
+		}
+	}
+
+	onDelete(id){
+		this.dialogService.openConfirmDialog('¿Estás seguro de eliminar el Motivo'+id+' ?').afterClosed().subscribe(res=>{
+			if (res==true) {
+				console.log(id);
+				this.snackBar.openSnackBar('Eliminado Correctamente','¿Deshacer?').onAction().subscribe(() => {
+				  console.log('Recuperado');
+				});
+			}else{
+				console.log(res);
+			}
+		});
+	}
+
 }
+
+
