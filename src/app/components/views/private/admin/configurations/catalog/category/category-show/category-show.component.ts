e1101e78e90cc4f3f294e5ff438c8f940bee0567
Anderson <andersondejesus2011@gmail.com>
2019-11-19 16:27:39 -0430

    acomodando motive

--- src/app/components/views/private/admin/configurations/catalog/category/category-show/category-show.component.ts
+++ src/app/components/views/private/admin/configurations/catalog/category/category-show/category-show.component.ts
@@ -2,47 +2,42 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import {NgForm} from '@angular/forms';
 
 import { MatTableDataSource } from '@angular/material/table';
-import { DialogService } from '../../../../../../../../core/services/dialog.service';
+import { DialogService } from 'src/app/core/services/dialog.service';
 
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Location} from '@angular/common';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
-import { Global } from '../../../../../../../../core/services/global';
-import { Category } from '../../../../../../../../models/category';
-import { Subcategory } from '../../../../../../../../models/subcategory';
-import { CategoryService } from '../../../../../../../../core/services/admin/category.service';
-import { SnackBarService } from '../../../../../../../../core/services/snack-bar.service';
-import { SubcategoryService } from '../../../../../../../../core/services/admin/subcategory.service';
+import { Global } from 'src/app/core/services/global';
+import { Category } from 'src/app/models/category';
+import { Subcategory } from 'src/app/models/subcategory';
+import { CategoryService } from 'src/app/core/services/admin/category.service';
+import { SnackBarService } from 'src/app/core/services/snack-bar.service';
+import { SubcategoryService } from 'src/app/core/services/admin/subcategory.service';
 
 @Component({
   selector: 'sib-category-show',
   templateUrl: './category-show.component.html',
   styleUrls: ['./category-show.component.scss']
 })
-export class CategoryShowComponent implements OnInit {
+export class CategoryShowComponent implements OnInit
+{
 
-	
+	public category:Category;
+	public arrayCategory:any;
+	public subcategories: Array < Subcategory > = new Array < Subcategory > ();
+	public message:string;
+	public failedConect:string;
 
-	  public category:Category;
-	  public arrayCategory:any;
-	  public subcategories: Array < Subcategory > = new Array < Subcategory > ();
-	  public message:string;
-	  public failedConect:string;
-	
-	  displayedColumns: string[] = ['name','description','status'];
-	  dataSource: MatTableDataSource<Subcategory>;
+	displayedColumns: string[] = ['name','description','status'];
+	dataSource: MatTableDataSource<Subcategory>;
 
-	  
-		displayedColumnsC: string[] = ['name', 'description','subcategories','status'];
-		dataSourceC: MatTableDataSource<Category>;
 
-		  
-	
- 
-	  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
+	displayedColumnsC: string[] = ['name', 'description','subcategories','status'];
+	dataSourceC: MatTableDataSource<Category>;
+
+	@ViewChild(MatPaginator) paginator: MatPaginator;
 
 	constructor
 	(
@@ -50,12 +45,12 @@ export class CategoryShowComponent implements OnInit {
 		private _route: ActivatedRoute,
 		private _router: Router,
 		private _location: Location,
-    	private snackBar: SnackBarService,
-    	private dialogService: DialogService,
-    )
-    {
+		private snackBar: SnackBarService,
+		private dialogService: DialogService,
+		)
+	{
 
-    }
+	}
 
 	ngOnInit()
 	{
@@ -66,7 +61,7 @@ export class CategoryShowComponent implements OnInit {
 				let id = params.id;
 				this.getCategory(id);
 			}
-		);
+			);
 	}
 
 
@@ -77,14 +72,14 @@ export class CategoryShowComponent implements OnInit {
 			response =>
 			{
 				if (response.status==true)
-        {
-          this.category = response.category;
-          this.arrayCategory = [];
-          this.arrayCategory.push(this.category);
-          console.log(this.category);
-          this.subcategories = response.category.subcategories;
-					
-          if(this.subcategories.length>0)
+				{
+					this.category = response.category;
+					this.arrayCategory = [];
+					this.arrayCategory.push(this.category);
+					console.log(this.category);
+					this.subcategories = response.category.subcategories;
+
+					if(this.subcategories.length>0)
 					{
 						console.log(this.subcategories);
 						this.table();
@@ -97,9 +92,9 @@ export class CategoryShowComponent implements OnInit {
 					}
 				}
 				else
-		        {
-		          console.log(response);
-		        }
+				{
+					console.log(response);
+				}
 			},
 			error =>
 			{
@@ -112,31 +107,29 @@ export class CategoryShowComponent implements OnInit {
 					}
 				}
 			}
-		)
+			)
 	}
 
-
-
-
 	applyFilter(filterValue: string)
-  {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+	{
+		this.dataSource.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
+		if (this.dataSource.paginator) {
+			this.dataSource.paginator.firstPage();
+		}
+	}
 
-  table()
-  {
-    this.dataSource = new MatTableDataSource(this.subcategories);
-    this.dataSourceC = new MatTableDataSource(this.arrayCategory);
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
-  goBack(){
-	this._location.back();
-  }
+	table()
+	{
+		this.dataSource = new MatTableDataSource(this.subcategories);
+		this.dataSourceC = new MatTableDataSource(this.arrayCategory);
+		this.dataSource.paginator = this.paginator;
+	}
+
+	goBack()
+	{
+		this._location.back();
+	}
 
 
 }
