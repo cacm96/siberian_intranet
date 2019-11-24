5c9f0861c6ffa70c46dd01b28a337c9710e22405
Dayana Suarez <dayana15.03suarez@gmail.com>
2019-11-24 02:13:01 -0400

    Questions

--- src/app/components/shared/add-calification-dialog/add-calification-dialog.component.ts
+++ src/app/components/shared/add-calification-dialog/add-calification-dialog.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { DialogService } from 'src/app/core/services/dialog.service';
+import { SnackBarService } from 'src/app/core/services/snack-bar.service';
+
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import {Location} from '@angular/common';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Global } from 'src/app/core/services/global';
+import { Question } from 'src/app/models/question';
+import { QuestionService } from 'src/app/core/services/admin/question.service';
 
 
 @Component({
@@ -8,15 +17,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCalificationDialogComponent implements OnInit {
 
-  
-  constructor()
-   { }
+  public questions:any;
+  public message:string;
+  public failedConect:string;
 
-  ngOnInit() {
-
+  constructor
+  (
+    private dialogService: DialogService,
+    private snackBar: SnackBarService,
+    private _questionService: QuestionService,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _location: Location
+  )
+  {
     
+  }
 
-    
+  ngOnInit()
+  {
+    this.getQuestions();
+  }
+
+  getQuestions()
+  {
+    this._questionService.All().subscribe
+    (
+      response =>
+      {
+        if (response.status==true)
+        {
+          this.questions = response.question;
+          console.log(this.questions);
+        }
+
+      },
+      error =>
+      {
+        console.log(<any>error);
+        if(error instanceof HttpErrorResponse)
+        {
+          if(error.status===0)
+          {
+            this.failedConect = Global.failed;
+          }
+        }
+      }
+      )
   }
 
  
