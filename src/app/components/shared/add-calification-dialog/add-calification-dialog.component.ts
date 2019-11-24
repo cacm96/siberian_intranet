591d83954f74fd6b354e1c1d1f8e5b77e4ab2ff8
Dayana Suarez <dayana15.03suarez@gmail.com>
2019-11-24 19:49:49 -0400

    otras cosas ahi no se

--- src/app/components/shared/add-calification-dialog/add-calification-dialog.component.ts
+++ src/app/components/shared/add-calification-dialog/add-calification-dialog.component.ts
@@ -1,13 +1,6 @@
-import { Component, OnInit, } from '@angular/core';
-import { DialogService } from 'src/app/core/services/dialog.service';
-import { SnackBarService } from 'src/app/core/services/snack-bar.service';
 
-import { Router, ActivatedRoute, Params } from '@angular/router';
-import {Location} from '@angular/common';
-import { HttpErrorResponse } from '@angular/common/http';
-import { Global } from 'src/app/core/services/global';
-import { Question } from 'src/app/models/question';
-import { QuestionService } from 'src/app/core/services/admin/question.service';
+import { Component, OnInit, } from '@angular/core';
+
 
 
 @Component({
@@ -17,55 +10,15 @@ import { QuestionService } from 'src/app/core/services/admin/question.service';
 })
 export class AddCalificationDialogComponent implements OnInit {
 
-  public questions:any;
-  public message:string;
-  public failedConect:string;
-
   constructor
   (
-    private dialogService: DialogService,
-    private snackBar: SnackBarService,
-    private _questionService: QuestionService,
-    private _route: ActivatedRoute,
-    private _router: Router,
-    private _location: Location
-  )
-  {
-    
+  	
+  ) { }
+
+
+  ngOnInit() {
   }
 
-  ngOnInit()
-  {
-    this.getQuestions();
-  }
 
-  getQuestions()
-  {
-    this._questionService.All().subscribe
-    (
-      response =>
-      {
-        if (response.status==true)
-        {
-          this.questions = response.question;
-          console.log(this.questions);
-        }
 
-      },
-      error =>
-      {
-        console.log(<any>error);
-        if(error instanceof HttpErrorResponse)
-        {
-          if(error.status===0)
-          {
-            this.failedConect = Global.failed;
-          }
-        }
-      }
-      )
-  }
-
- 
-  
 }
