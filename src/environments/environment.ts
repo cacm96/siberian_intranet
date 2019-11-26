6b4841141f49478fda59fa0ca8da3c28e3a20e31
Anderson <andersondejesus2011@gmail.com>
2019-11-26 15:10:19 -0430

    api local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  api: 'https://siberian-api.herokuapp.com/api/',
-  //api: 'http://localhost:12224/api/',
+  //api: 'https://siberian-api.herokuapp.com/api/',
+  api: 'http://localhost:12224/api/',
   imgURL: 'http://localhost:12225/',
 };
 
