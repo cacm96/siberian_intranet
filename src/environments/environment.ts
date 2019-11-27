63c99f3cd87cccd371a10580cd531c6cc85afed2
Anderson <andersondejesus2011@gmail.com>
2019-11-26 23:14:50 -0430

    cambios en app.module nuevos con modif de ely

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //api: 'https://siberian-api.herokuapp.com/api/',
-  api: 'http://localhost:12224/api/',
+  api: 'http://localhost:12225/api/',
   imgURL: 'http://localhost:12225/',
 };
 
